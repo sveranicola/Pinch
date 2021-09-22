67c1d953827db8e64cc47da61efc2607bcce7ecc
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-09-22 13:03:29 -0400

    please work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 
 function App() {
   // eslint-disable-next-line
   const [state, setState] = useState({ state: ' ' });
-=======
->>>>>>> bfbcaf1f623555e14869ae43e92ccd8492204f90
 
   return (
     <div>
--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,6 @@ import App from './components/App.jsx';
 
 ReactDOM.render(
   <App />,
-<<<<<<< HEAD:src/index.jsx
   // eslint-disable-next-line no-undef
-=======
->>>>>>> bfbcaf1f623555e14869ae43e92ccd8492204f90:src/index.js
   document.getElementById('app'),
 );
