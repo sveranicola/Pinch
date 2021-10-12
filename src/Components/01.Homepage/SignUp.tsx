f7c118f5941b9457bffc7baaa5f6f15657dded11
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-10-12 15:16:55 -0400

    some progress on sign up box

---
+++ src/Components/01.Homepage/SignUp.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+function SignUp() {
+  return (
+    <div className="sign-up">
+      <img src="https://i.imgur.com/MI5gkUq.png" alt="smallLogo" className="logo-smoll" />
+      <div className="column-words">
+        <div className="underline">Free</div>
+        <div className="underline">Secure</div>
+        <div>Reliable</div>
+      </div>
+      <div className="column-description">
+        <div>Sign up for Pinch!</div>
+        <p>Start reaching your financial goals with powerful tools,</p>
+        <p>personalized analytics, and more. Fincancial freedom has</p>
+        <p>never been so easy to do!</p>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
