1e1eed11f313e2948bd21c568c1afb441916fb2b
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-10-13 14:03:26 -0400

    sign up box

--- src/Components/01.Homepage/SignUp.tsx
+++ src/Components/01.Homepage/SignUp.tsx
@@ -3,17 +3,21 @@ import * as React from 'react';
 function SignUp() {
   return (
     <div className="sign-up">
-      <img src="https://i.imgur.com/MI5gkUq.png" alt="smallLogo" className="logo-smoll" />
-      <div className="column-words">
-        <div className="underline">Free</div>
-        <div className="underline">Secure</div>
-        <div>Reliable</div>
+      <div className="container-logo-words">
+        <img src="https://i.imgur.com/MI5gkUq.png" alt="smallLogo" className="logo-smoll" />
+        <div className="column-words">
+          <div className="underline">Free</div>
+          <div className="underline">Secure</div>
+          <div>Reliable</div>
+        </div>
       </div>
       <div className="column-description">
-        <div>Sign up for Pinch!</div>
-        <p>Start reaching your financial goals with powerful tools,</p>
-        <p>personalized analytics, and more. Fincancial freedom has</p>
-        <p>never been so easy to do!</p>
+        <div className="make-the-button-center">
+          <div className="button-sign-up">Sign up for Pinch!</div>
+        </div>
+        <p className="pargaph-size">Start reaching your financial goals with powerful tools,</p>
+        <p className="pargaph-size">personalized analytics, and more. Fincancial freedom has</p>
+        <p className="pargaph-size">never been so easy to do!</p>
       </div>
     </div>
   );
