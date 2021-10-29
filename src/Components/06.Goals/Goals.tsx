8d4bc973e072f753bcbf5a67b4debd84eb38f3cf
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-10-29 00:16:05 -0400

    scss progress

--- src/Components/06.Goals/Goals.tsx
+++ src/Components/06.Goals/Goals.tsx
@@ -1,9 +1,48 @@
 import * as React from 'react';
+import { FaRegEdit } from 'react-icons/fa';
+import { GrClose } from 'react-icons/gr';
+import { BsPiggyBank } from 'react-icons/bs';
+import { IoIosAddCircleOutline } from 'react-icons/io';
 
 function Goals() {
   return (
-    <div>
-      Goals
+    <div className="goals-page">
+      <div className="goals-list">
+        <div className="current-goals">
+          <div>
+            YOUR CURRENT GOALS
+          </div>
+          <BsPiggyBank size={25} />
+        </div>
+        <div>
+          goals end up here
+        </div>
+        <div className="add-new">
+          <div>
+            Add a new Goal
+          </div>
+          <IoIosAddCircleOutline size={25} />
+        </div>
+      </div>
+      <div className="goals-analytics">
+        <div className="current-goal-title">
+          <div>
+            GOAL NAME
+          </div>
+          <div className="icon-style">
+            <FaRegEdit size={25} color="#696969" />
+            <GrClose size={25} color="#696969" />
+          </div>
+        </div>
+        <div className="chart-space">
+          <div className="description">
+            Description
+          </div>
+          <div className="forecast">
+            Forecast
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/06.Goals/exampleGoals.ts
@@ -0,0 +1,22 @@
+const exampleGoals = {
+  userID: 12334,
+  goals: {
+    1: {
+      name: 'Rainy Day Fund',
+      goalAmount: 10000.00,
+      currentAmount: 7157.03,
+    },
+    2: {
+      name: 'New Car Fund',
+      goalAmount: 8100.00,
+      currentAmount: 957.00,
+    },
+    3: {
+      name: 'PS5 Fund',
+      goalAmount: 1000.00,
+      currentAmount: 117.83,
+    },
+  },
+};
+
+export default exampleGoals;
